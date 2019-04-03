a46471e01232b2dc93f75b9c793d6d78bdb5f56e
Wiliam Ferraciolli <wiliam334@hotmail.com>
2019-04-03 20:44:52 +0100

    Finished Sales API added CORS

--- salesAPI/src/main/java/com/wiltech/sales/resource/ClientResource.java
+++ salesAPI/src/main/java/com/wiltech/sales/resource/ClientResource.java
@@ -3,12 +3,14 @@ package com.wiltech.sales.resource;
 import com.wiltech.sales.model.Client;
 import com.wiltech.sales.repository.ClientRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/clients")
 public class ClientResource {
--- salesAPI/src/main/java/com/wiltech/sales/resource/ProductResource.java
+++ salesAPI/src/main/java/com/wiltech/sales/resource/ProductResource.java
@@ -3,12 +3,14 @@ package com.wiltech.sales.resource;
 import com.wiltech.sales.model.Product;
 import com.wiltech.sales.repository.ProductRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/products")
 public class ProductResource {
