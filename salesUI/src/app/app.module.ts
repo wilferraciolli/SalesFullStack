154a5e75e2ab18347e0470b0d64269232d79e09a
Wiliam Ferraciolli <wiliam334@hotmail.com>
2019-04-03 23:09:55 +0100

    Added table to list sales

--- salesUI/src/app/app.module.ts
+++ salesUI/src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
+import { TableModule } from 'primeng/table';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -16,6 +18,7 @@ import { SalesListComponent } from './sales-list/sales-list.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    TableModule,
     AppRoutingModule
   ],
   providers: [],
